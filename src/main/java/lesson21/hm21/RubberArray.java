740d6713f5af72e3922f5ecebb0ae860f49e8dc6
HalynaPoryvaieva <lastiklastik407@gmail.com>
2024-03-04 16:12:19 +0100

    Hw21

---
+++ src/main/java/lesson21/hm21/RubberArray.java
@@ -0,0 +1,90 @@
+package lesson21.hm21;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic,Hw21,Task 1
+ * @version 4-Mar-24
+ * @author Halyna Poryvaieva
+ */
+public class RubberArray {
+
+    private final int INIT_DATA_SIZE = 3;
+    private final float EXTEND_INDEX = 1.5f;
+
+    private int[] data;
+
+    private int length;
+
+    public RubberArray() {
+        data = new int[INIT_DATA_SIZE];
+        length = 0;
+    }
+
+    public int get(int idx) {
+        return data[idx];
+    }
+
+    private void extendArrayIfNeed() {
+        if (length == data.length) {
+            int[] newData = new int[(int) (data.length * EXTEND_INDEX)];
+            for (int i = 0; i < data.length; i++) {
+                newData[i] = data[i];
+            }
+            data = newData;
+        }
+    }
+
+    public void add(int value) {
+        extendArrayIfNeed();
+        data[length] = value;
+        length++;
+    }
+
+    //Task 4
+    public void add(int value, int idx) {
+        extendArrayIfNeed();
+        for (int i = length; i > idx; i--) {
+            data[i] = data[i - 1];
+        }
+        data[idx] = value;
+        length++;
+    }
+
+    public void remove(int idx) {
+        for (int i = idx; i < data.length - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        length--;
+    }
+
+    //Task2
+    public boolean contains(int value) {
+        return indexOf(value) !=-1;
+    }
+
+    //Task3
+    public int indexOf(int value) {
+        for (int i = 0; i < length; i++) {
+            if (data[i] == value) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //Task1
+    @Override
+    public String toString() {
+        StringBuilder strSb = new StringBuilder();
+        strSb.append("[");
+        for (int i = 0; i < length; i++) {
+            strSb.append(data[i]);
+            if (i < length - 1) {
+                strSb.append(",");
+            }
+        }
+        strSb.append("]");
+        return strSb.toString();
+    }
+}
+
+
---
+++ src/test/java/lesson21/RubberArrayTest.java
@@ -0,0 +1,57 @@
+package lesson21;
+
+import lesson21.hm21.RubberArray;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * AIT-TR, cohort 42.1, Java Basic,Hw21,Task 1
+ * @version 4-Mar-24
+ * @author Halyna Poryvaieva
+ */
+public class RubberArrayTest {
+
+    public RubberArray rubberArray;
+
+    @BeforeEach
+    public void init(){
+       rubberArray=new RubberArray();
+        rubberArray.add(9);
+       rubberArray.add(5);
+        rubberArray.add(7);
+    }
+
+    //Task1
+    @Test
+    public void testToString() {
+        StringBuilder strSb = new StringBuilder();
+        strSb.append("[1,2,3]");
+        String expected = "[1,2,3]";
+        String actual = strSb.toString();
+        Assertions.assertEquals(expected, actual);
+    }
+    //Task2
+    @Test
+    public void containsTest(){
+        int  expected=9;
+        boolean actuale =rubberArray.contains(expected);
+        Assertions.assertTrue(actuale);
+    }
+    //Task3
+    @Test
+    public void indexOfTest(){
+        int  expected=0;
+        int actual=rubberArray.indexOf(9);
+        Assertions.assertEquals(expected,actual);
+    }
+    //Task4
+    @Test
+    public void addTest(){
+        int value=1;
+        int index=2;
+        rubberArray.add(1,2);
+        Assertions.assertEquals(value,rubberArray.get(index));
+        Assertions.assertEquals(9,rubberArray.toString().length());
+    }
+}
